2ee47562f09643e03a1851f5960705fc56370074
Hector <torrezhectorb@gmail.com>
2023-10-02 18:27:56 -0600

    Blog Schema

---
+++ models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+    min: 3
+  },
+  author: {
+    type: String,
+    require: true,
+    min: 3
+  }
+
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
