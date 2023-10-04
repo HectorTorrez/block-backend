9e69c49d2d593588f60180a40b6080182ddb0915
Hector <torrezhectorb@gmail.com>
2023-10-03 18:54:32 -0600

    configuration to use cross-env

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const { MONGODB_URI } = require('../utils/config')
 require('dotenv').config()
 
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false)
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(MONGODB_URI)
     console.log('Connected to MongoDB')
   } catch (error) {
     console.log('Error to connect', error.message)
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./app')
+const { PORT, MONGODB_URI } = require('./utils/config')
 
-app.listen(process.env.PORT, () => {
-  console.log(`Connected to port ${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`Connected to port ${PORT}`)
+  console.log(MONGODB_URI)
 })
