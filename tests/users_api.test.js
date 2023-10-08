258d3fad6ea48bb8a9401ab4e31d64c50f4a16c1
Hector <torrezhectorb@gmail.com>
2023-10-07 19:36:44 -0600

    test/ new test for the new enpoints

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const app = require('../app.js')
 const api = supertest(app)
 
-describe('post/ create a user', () => {
+describe('API EndPoints', () => {
   beforeEach(async () => {
     await User.deleteMany({})
 
@@ -16,7 +16,18 @@ describe('post/ create a user', () => {
     await user.save()
   })
 
-  test('must be created a user', async () => {
+  test('GET /api/users/ one user must be', async () => {
+    const usersAtStart = await helper.usersInBD()
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInBD()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test('POST /api/users/ an user must be created ', async () => {
     const usersAtStart = await helper.usersInBD()
 
     const newUser = {
@@ -34,6 +45,57 @@ describe('post/ create a user', () => {
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
     const users = usersAtEnd.map(u => u.username)
     expect(users).toContain(newUser.username)
+  })
+
+  test('PATH /api/users an user must be updated', async () => {
+    const usersAtStart = await helper.usersInBD()
+    const userToUpdate = usersAtStart[0]
+    const user = { username: 'hector00', name: 'Burrito', password: 'antonio' }
+    await api
+      .patch(`/api/users/${userToUpdate.id}`)
+      .send(user)
+      .expect(200)
+
+    const usersAtEnd = await helper.usersInBD()
+    const userUpdated = usersAtEnd[0]
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    expect(userUpdated.username).toContain(user.username)
+    expect(userUpdated.name).toContain(user.name)
+  })
+
+  test('PATH /api/users an user must be not updated if not have password', async () => {
+    const usersAtStart = await helper.usersInBD()
+    const userToUpdate = usersAtStart[0]
+    const user = { username: 'hector00', name: 'Burrito' }
+    await api
+      .patch(`/api/users/${userToUpdate.id}`)
+      .send(user)
+      .expect(400)
+    const usersAtEnd = await helper.usersInBD()
+    const userUpdated = usersAtEnd[0]
+    expect(userUpdated.username).toContain(user.username)
+    expect(userUpdated.name).not.toContain(user.name)
+  })
+
+  test('DELETE /api/users one user must be deleted', async () => {
+    const usersAtStart = await helper.usersInBD()
+    const userToDelete = usersAtStart[0]
+    await api
+      .delete(`/api/users/${userToDelete.id}`)
+      .expect(204)
+
+    const usersAtEnd = await helper.usersInBD()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length - 1)
+  })
+  test('DELETE /api/users a user should not be deleted if the user id is invalid', async () => {
+    const usersAtStart = await helper.usersInBD()
+    const id = 'aewqe1231231231231232'
+    await api
+      .delete(`/api/users/${id}`)
+      .expect(400)
+
+    const usersAtEnd = await helper.usersInBD()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
