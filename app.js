8ac2df5cf3ae41b66c0b367719a7392fb2c06d8a
Hector <torrezhectorb@gmail.com>
2023-10-10 18:47:28 -0600

    controller/ new login

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const { connectDB } = require('./config/connectDB')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 connectDB()
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
